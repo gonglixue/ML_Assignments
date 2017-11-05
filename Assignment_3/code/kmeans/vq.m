9e4eec5dc79da0d7dd30b382eba5a7ee3c1d3a5f
gonglixue <724351282@qq.com>
2017-11-05 21:56:32 +0800

    section 4 report

--- Assignment_3/code/kmeans/vq.m
+++ Assignment_3/code/kmeans/vq.m
@@ -1,9 +1,13 @@
-img = imread('sample1.jpg');
+img = imread('aa.jpg');
 fea = double(reshape(img, size(img, 1)*size(img, 2), 3));
-% YOUR (TWO LINE) CODE HERE
-K = 10;
-[idx, ctrs, iter_ctrs] = kmeans(fea,K);
-kmeans_plot(fea, idx, ctrs, iter_ctrs)
-
-fea = ctrs(idx,:);
+figure;
 imshow(uint8(reshape(fea, size(img))));
+% YOUR (TWO LINE) CODE HERE
+K = [8,16,32,64];
+for i = 1:length(K)
+   [idx, ctrs, iter_ctrs] = kmeans(fea, K(i));
+   fea_out = ctrs(idx,:);
+   figure;
+   imshow(uint8(reshape(fea_out, size(img))));
+end
+
